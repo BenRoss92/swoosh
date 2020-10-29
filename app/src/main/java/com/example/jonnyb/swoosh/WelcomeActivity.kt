0513348666313a2162695316b1db3f2f82a6e377
Ben Ross <benjamin.ross@youview.com>
2020-10-29 15:16:56 +0000

    Transition from Welcome page to League page

--- app/src/main/java/com/example/jonnyb/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/jonnyb/swoosh/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.example.jonnyb.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            // send an intent (by creating it) that will contain both this activity's context information and the particular class (i.e. activity) that we want to target (and transition to).
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
