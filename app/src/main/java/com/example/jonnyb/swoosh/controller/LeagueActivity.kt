8731518c6757fb694665a68dc52cd6e37f4bbabc
Ben Ross <benjamin.ross@youview.com>
2021-01-07 16:16:50 +0000

    Lesson 55 - Instance state
    * Keep the skill and league options saved, even when screen is rotated
    * Change default app icon to a relevant image

--- app/src/main/java/com/example/jonnyb/swoosh/controller/LeagueActivity.kt
+++ app/src/main/java/com/example/jonnyb/swoosh/controller/LeagueActivity.kt
@@ -13,6 +13,19 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    // Activities get destroyed when the screen orientation changes (i.e. when the screen is rotated). Any data saved in memory, e.g. our Player instance, will be destroyed when the screen is rotated. To prevent a new empty instance of Player being created when this happens, we serialise and store the player instance from our old activity instance (the one about to be destroyed), adding it to the new version of state (outState) which will then be applied to the new Activity instance. onSaveInstanceState is called before the activity is destroyed. onRestoreInstanceState is called when the activity is recreated. The same instance of the Bundle object is passed into both of these lifecycle methods. The Bundle can be null if the activity has not been destroyed yet, so null handling is needed. We then get and deserialise the Player instance fro the new state and save it to a property inside of our new activity instance (the 'player' property). The result is that the user's league selection is remembered, even if the screen is rotated after the selection is made.
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
--- app/src/main/java/com/example/jonnyb/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/example/jonnyb/swoosh/controller/SkillActivity.kt
@@ -14,6 +14,19 @@ class SkillActivity : BaseActivity() {
     // We want this variable to be available to all class methods, but we can't assign a value until the activity is created. We can use 'lateinit' to promise that we will initialise this Player instance later on when onCreate is called (i.e. when the activity is initialised).
     lateinit var player : Player
 
+    // Overriding onSaveInstanceState and onRestoreInstanceState lifecycle methods to save player information if screen is rotated - see comment in LeagueActivity for explanation.
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
